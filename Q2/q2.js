9323c9e394e08b503c02439d279ec58bd7e307a2
Leonardo Dutra <leonardo.uece@gmail.com>
2016-04-07 21:30:09 -0300

    questão de número 2 foi resolvida

---
+++ Q2/q2.js
@@ -0,0 +1,3 @@
+function question2() {
+	alert("Padrão de Projeto Singleton, que consiste em uma classe gerenciar a própria instância dela, além de evitar que qualquer outra classe crie uma instância dela.");
+}
